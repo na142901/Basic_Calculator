2a027bc291d4f5ab5311d58eed94861243cdd044
Numan Ansari <82473560+na142901@users.noreply.github.com>
2025-03-24 17:00:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+ let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+       if (display.innerText === '0') {
+              display.innerText = value;
+       } else {
+              display.innerText += value;
+       }
+}
+function clearDisplay() {
+       display.innerText = '0';
+}
+function calculateResult() {
+       try {
+              display.innerText = eval(display.innerText);
+       } catch {
+              display.innerText = 'Error';
+       }
+};
